76554d7bc6e2d9925e37457514ba850dae73fb34
Chris <0355882@student.rocvantwente.nl>
2024-04-03 15:30:26 +0200

    finished

--- WordSearcher/Class1.cs
+++ WordSearcher/Class1.cs
@@ -4,27 +4,42 @@
     {
         public bool Exist(char[][] board, string word)
         {
-            for (int x = 0; x < board.Length; x++)
-            {
-                for (int y = 0; y < board[x].Length; y++)
+            int m = board.Length;
+            int n = board[0].Length;
+
+            Func<int, int, int, bool> backtrack = null;
+            backtrack = (i, j, k) => {
+                if (k == word.Length)
                 {
-                    if (board[x][y] == word[0])
+                    return true;
+                }
+                if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k])
+                {
+                    return false;
+                }
+
+                char temp = board[i][j];
+                board[i][j] = '\0';
+                bool result = backtrack(i + 1, j, k + 1) ||
+                              backtrack(i - 1, j, k + 1) ||
+                              backtrack(i, j + 1, k + 1) ||
+                              backtrack(i, j - 1, k + 1);
+
+                board[i][j] = temp;
+                return result;
+            };
+
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    if (backtrack(i, j, 0))
                     {
-                        for (int i = 1; i < word.Length; i++)
-                        {
-                            if (!checkAdjacent(word[i], board[x - 1][y], board[x + 1][y], board[]))
-                            {
-                                break;
-                            }
-                        }
+                        return true;
                     }
                 }
             }
-        }
-
-        public bool checkAdjacent(char letter, char? up, char? down, char? left, char? right)
-        {
-
+            return false;
         }
     }
 }
